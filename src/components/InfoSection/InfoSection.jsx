faf7fba0fadbc40cfa5d0d93c422c9baf818faee
Majid <majid.eltayeb@gmail.com>
2021-08-14 13:43:11 +0200

    Add infoSection Structure

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,9 +1,30 @@
 import React from 'react';
+import AvatarImg from './avatar.png';
+import './InfoSection.css';
 const InfoSection = () => {
-    return <div>
-        InfoSection 
-
-    </div>
+    // Avatar
+    // Info
+        
+    return (<div className="infoSection">
+        <div className="avatar">
+            <img src={AvatarImg} alt="avatar" />
+        </div>
+        <div className="info">
+            <div className="actionButtons">
+            <div>Username</div>
+            <div>button</div>
+            <div>svg button</div>
+            </div>
+             <div className="links">
+                 <div className="">Link</div>
+                 <div className="">Link</div>
+                 <div className="">Link</div>
+             </div>
+             <div className="displayName">
+                DisplayName
+             </div>
+        </div>
+    </div>)
 }
 
 export default InfoSection;
