e8b4a6ee1667f115b0cd4a14ef367158df5fedc8
Ziad Bastawy <ziadmahmoud413@gmail.com>
2021-08-14 14:36:18 +0200

    fix conflict

--- src/components/InfoSection/InfoSection.jsx
+++ src/components/InfoSection/InfoSection.jsx
@@ -1,38 +1,37 @@
 import React from 'react';
 import AvatarImg from './avatar.png';
+import SettingIcon from './setting.js';
 import './InfoSection.css';
 const InfoSection = () => {
     // Avatar
     // Info
-        
+
     return (<div className="infoSection">
         <div className="avatar">
             <img src={AvatarImg} alt="avatar" />
         </div>
         <div className="info">
             <div className="actionButtons">
-            <div>Username</div>
-            <div>button</div>
-            <div>svg button</div>
+                <div className="userName">Username</div>
+                <div>
+                    <button className="editButton">
+                        Edit Profile
+                    </button>
+                </div>
+                <div>
+                    <a className="settingButton" href="#">
+                        <SettingIcon />
+                    </a>
+                </div>
             </div>
-             <div className="links">
-                 
-                 <div className="">
-                 <a href="" target="self"><b>384</b> posts</a>
-                 </div>
-
-                 <div className="">
-                 <a href="" target="self"><b>582</b> followers</a>
-                 </div>
-
-                 <div className="">
-                 <a href="" target="self"><b>929</b> folowing</a>
-                 </div>
-
-             </div>
-             <div className="displayName">
-               <h3>Yara Refaee</h3>
-             </div>
+            <div className="links">
+                <a className="link" href="#" ><b>384</b> posts</a>
+                <a className="link" href="#" ><b>582</b> followers</a>
+                <a className="link" href="#" ><b>929</b> folowing</a>
+            </div>
+            <div className="displayName">
+                <h3>Yara Refaee</h3>
+            </div>
         </div>
     </div>)
 }
