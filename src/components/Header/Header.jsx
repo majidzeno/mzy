87d9687de65d629915df0be48c6a7f3085740259
olaalnaggar <ola_alnaggar@yahoo.com>
2021-08-18 08:24:39 +0200

    Add styles to header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -6,28 +6,22 @@ import Direction from './Directiom.png';
 import Love from './heart.png';
 import Avatar from './avatar.png';
 import './Header.css';
-
 const Header = () => {
     return (<div className="HeaderSecion">
-        <div className="instaLogo">
+        <div className="InstaLogo">
             <img src={InstaLogo} alt="Logo" width="120px" />
         </div>
         <div className="Search">
             <input type="text" className="SearchBar" placeholder="search..."></input>
         </div>
-        <div className="icons">
+        <div className="Icons">
             <a href="" target="self">
-                <img src={Home} className="icon" alt="Home" width="23px" /></a>
-
-            <a href="" target="self">  <img src={Share} className="icon" alt="Share" width="23px" /></a>
-            <a href="" target="self"> <img src={Direction} className="icon" alt="Direction" width="23px" /></a>
-            <a href="" target="self">  <img src={Love} className="icon" alt="Love" width="23px" /></a>
-            <a href="" target="self">  <img src={Avatar} className="icon avatarIcon" alt="Avatar" width="25px" /></a>
-
+                <img src={Home} className="Icon" alt="Home" width="23px" /></a>
+            <a href="" target="self">  <img src={Share} className="Icon" alt="Share" width="23px" /></a>
+            <a href="" target="self"> <img src={Direction} className="Icon" alt="Direction" width="23px" /></a>
+            <a href="" target="self">  <img src={Love} className="Icon" alt="Love" width="23px" /></a>
+            <a href="" target="self">  <img src={Avatar} className="Icon AvatarIcon" alt="Avatar" width="25px" /></a>
         </div>
-
-
     </div>)
 }
-
 export default Header;
