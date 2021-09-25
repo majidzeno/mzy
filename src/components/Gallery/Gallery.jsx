65d6483b87f079f88e31e7358d1ae3627d59afdd
Ziad Bastawy <ziadmahmoud413@gmail.com>
2021-09-25 08:27:50 +0200

    add images to gallery

--- src/components/Gallery/Gallery.jsx
+++ src/components/Gallery/Gallery.jsx
@@ -1,9 +1,22 @@
-import React from 'react';
+import React from "react";
+import './Gallery.css'
 const Gallery = () => {
-    return <div>
-        Gallery 
-
-    </div>
-}
+  return (
+    <>
+      <div className="Gallery">
+        {[...Array(10)].map((x,i) => (
+          <img
+            key={i}
+            className="Gallery-Image"
+            src="https://dummyimage.com/320x250/000/fff.jpg"
+            alt="img"
+          />
+          // <div>
+          // </div>
+        ))}
+      </div>
+    </>
+  );
+};
 
 export default Gallery;
